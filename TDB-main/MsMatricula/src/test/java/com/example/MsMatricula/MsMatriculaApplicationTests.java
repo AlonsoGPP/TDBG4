95fdf3d719455dbf383e11a63b8de7a2ba2866f5
AlonsoGPP <42708290+AlonsoGPP@users.noreply.github.com>
2023-12-07 06:38:50 -0500

    Añadiendo Ms-matricula.

---
+++ TDB-main/MsMatricula/src/test/java/com/example/MsMatricula/MsMatriculaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MsMatricula;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMatriculaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
