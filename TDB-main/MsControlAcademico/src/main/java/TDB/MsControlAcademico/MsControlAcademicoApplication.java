e7cd68a56e0122da9166cf6ce4c4e71e47a191b7
AlonsoGPP <42708290+AlonsoGPP@users.noreply.github.com>
2023-12-08 20:57:09 -0500

    Se añade el msControl academico

---
+++ TDB-main/MsControlAcademico/src/main/java/TDB/MsControlAcademico/MsControlAcademicoApplication.java
@@ -0,0 +1,13 @@
+package TDB.MsControlAcademico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsControlAcademicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsControlAcademicoApplication.class, args);
+	}
+
+}
