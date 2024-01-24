b6bf8283af807c4fa5ee0f8db60bcb82811671e1
NeilAnayaC <122710580+NeilAnayaC@users.noreply.github.com>
2024-01-24 00:31:01 -0500

    Grupo docentes controller, mapper

--- TDB-main/MsDocencia/src/main/java/com/Msdocencia/MsDocencia/dtos/GrupoMapper.java
+++ TDB-main/MsDocencia/src/main/java/com/Msdocencia/MsDocencia/dtos/GrupoMapper.java
@@ -1,4 +1,19 @@
 package com.Msdocencia.MsDocencia.dtos;
 
+
+import org.mapstruct.factory.Mappers;
+
+import com.Msdocencia.MsDocencia.model.GruposModel;
+
+
+
 public interface GrupoMapper {
+
+    GrupoMapper  mapper= Mappers.getMapper(GrupoMapper.class);
+
+    GrupoResponse modelToResponse(GruposModel gruposModel);
+
+    GruposModel requestToModel(GrupoRequest gruposRequest);
+
 }
+
--- TDB-main/MsDocencia/src/main/java/com/Msdocencia/MsDocencia/services/GrupoService.java
+++ TDB-main/MsDocencia/src/main/java/com/Msdocencia/MsDocencia/services/GrupoService.java
@@ -1,11 +1,10 @@
 package com.Msdocencia.MsDocencia.services;
 
-import com.Msdocencia.MsDocencia.repository.IGrupoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Service;
+
 
 @Service
 public class GrupoService {
-    @Autowired
-    IGrupoRepository iGrupoRepository;
+
 }
